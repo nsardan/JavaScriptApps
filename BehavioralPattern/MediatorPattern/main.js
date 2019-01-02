ba94e2fbb5575be72ee012d911059ab2ceb75c88
nsardana <neha.nsit@gmail.com>
2019-01-01 20:36:59 -0500

    Pluralsight javascript design patterns

--- BehavioralPattern/MediatorPattern/main.js
+++ BehavioralPattern/MediatorPattern/main.js
@@ -21,6 +21,38 @@ var auditingService = function(){
   }
 };
 
+var mediator = (function (){
+  var channels = { };
+  var subscribe = function(channel, context, func){
+    if(!mediator.channels[channel]){
+      mediator.channels[channel] = []
+    }
+    mediator.channels[channel].push({
+        context: context,
+        func: func
+    });
+  };
+
+  var publish = function(channel){
+    if(!this.channels[channel]){
+      return false;
+    }
+
+    var args = Array.prototype.slice.call(arguments, 1);
+
+    for(var i = 0; i < mediator.channels[channel].length; i++){
+      var sub = mediator.channels[channel][i];
+      sub.func.apply(sub.context, args);
+    }
+  }
+  return {
+    channels: {},
+    subscribe: subscribe,
+    publish: publish
+  };
+}());
+
+
 function ObserverList(){
   this.observerList = [];
 };
@@ -80,19 +112,23 @@ ObservableTask.prototype.removeObserver = function(observer){
   this.observers.removeAt(this.observers.indexOf(observer , 0));
 }
 
-var task1 = new ObservableTask({name: 'create a demo for constructors', user: 'Neha'});
+var task1 = new ObservableTask({
+              name: 'create a demo for mediators',
+              user: 'Neha'
+});
 
 
 var not = new notificationService();
 var ls = new loggingService();
 var audit = new auditingService();
 
-task1.addObserver(not.update);
-task1.addObserver(ls.update);
-task1.addObserver(audit.update);
+mediator.subscribe('complete', not, not.update);
+mediator.subscribe('complete', ls, ls.update);
+mediator.subscribe('complete', audit, audit.update);
 
-task1.save();
+task1.complete = function(){
+  mediator.publish('complete', this);
+  Task.prototype.complete.call(this);
+}
 
-
-task1.removeObserver(audit.update);
-task1.save();
+task1.complete();
